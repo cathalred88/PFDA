512349f6f69675f9248bd3acbe35325ac3300027
Cathal <cathalred88@hotmail.com>
2025-12-09 23:47:15 +0000

    assignment 2 list of holidays working

--- assignments/assignment02-bankholdiays.py
+++ assignments/assignment02-bankholdiays.py
@@ -8,11 +8,49 @@
 import requests  
 import json
 
+# Request from Web
+# this method downloads the file from the web new each time
 url =" https://www.gov.uk/bank-holidays.json" 
-response = requests.get(url) 
-dict = response.json() 
+r = requests.get(url) 
 
-print(json.dumps(dict['northern-ireland']['events'], indent = 4))  ## This is working code, dont fuck with it! 
+# Check the status of the request
+# if the server sucessfully handles the request, this will respond with the value "200"
+#print(r.status_code) 
 
-#print ( ['events'] for ['northern-ireland'] in dict if(['date'] == ['2027-05-31'])else: () )
-#print (obj for obj in dict if(obj['type'] == 1))
+#Alternatively, we can access the file locally: 
+FILENAME = "bankholidays.json"
+DATADIR = "../my-work/"
+FULLPATH = DATADIR + FILENAME
+
+# function to read the json into a Dict type object
+with open (FULLPATH, "rt") as fp:
+    bankholidays = json.load(fp)
+
+#print the data from the dict
+#print(bankholidays['northern-ireland']['events'])
+
+# call the event titles from the lists
+NIevents = bankholidays['northern-ireland'].get('events')
+#print(type(NIevents))
+for titles in NIevents:
+    print(list(titles.values())[0],":",list(titles.values())[1])
+
+
+#for dates in titles:
+    #list(dates.keys())[0]
+    #print(dates)
+
+#titles = events.items()
+#print(titles)
+
+#dict = r.json() 
+#bankholidays = json.dumps(dict['northern-ireland']['events'], indent = 4)
+#bankholidays = json.dumps(dict)
+#print(type(bankholidays))
+
+#print(dict['northern-ireland']['events'])
+
+#print(json.dumps(dict['northern-ireland']['events']['title'], indent = 4))  # This is working code, dont mess with it! 
+
+#print(json.dumps(dict['events'] for ['northern-ireland'] in dict if(['date'] == ['2027-05-31'])else: () ))
+#print(obj for obj in dict if(obj['type'] == 1))
