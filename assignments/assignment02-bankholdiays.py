0ede216adaacb1d937173e305b06dc3776f44667
Cathal <cathalred88@hotmail.com>
2025-12-10 22:58:53 +0000

    list compare is working

--- assignments/assignment02-bankholdiays.py
+++ assignments/assignment02-bankholdiays.py
@@ -31,40 +31,55 @@ with open (FULLPATH, "rt") as fp:
 
 # call the event titles that happen in Norther ireland from the lists
 NIevents = bankholidays['northern-ireland'].get('events')
-print("\nNorthern Ireland List:\n")
+#print("\nNorthern Ireland List:\n")
 NIlist = []
 for titles in NIevents:
     y=list(titles.values())[0],list(titles.values())[1]
     NIlist.append(y)
-print(NIlist)
+#print(NIlist)
 with open("Northern-Ireland Bank Holidays.txt","w") as output:
     output.write(str(NIlist))
 
 
 #Scotland
 Scotlandevents = bankholidays['scotland'].get('events')
-print("\nScotland List:\n")
+#print("\nScotland List:\n")
 ScotlandList =[]
 for titles in Scotlandevents:
     y=list(titles.values())[0],":",list(titles.values())[1]
     ScotlandList.append(y)
-print(ScotlandList)
+#print(ScotlandList)
 with open("Scotland Bank Holidays.txt","w") as output:
     output.write(str(ScotlandList))
 
 #England and Wales
 EnglandAndWalesevents = bankholidays['england-and-wales'].get('events')
-print("\nEngland and Wales List:\n")
+#print("\nEngland and Wales List:\n")
 EnglandAndWalesList = []
 for titles in EnglandAndWalesevents:
     y=list(titles.values())[0],":",list(titles.values())[1]
     EnglandAndWalesList.append(y)
-print(EnglandAndWalesList)
+#print(EnglandAndWalesList)
 with open("England and Wales Bank Holidays.txt","w") as output:
     output.write(str(EnglandAndWalesList))
 
 
 # now we just need a way to detect the unique individual elements from these 3 text files. 
+# i will acomplish this using Sets # https://stackoverflow.com/questions/64422967/how-to-get-values-unique-to-each-list-for-three-lists
+set_NI = set(NIlist)
+set_Scot = set(ScotlandList)
+set_EngWales = set(EnglandAndWalesList)
+
+onlyinNI = set_NI - set_Scot - set_EngWales
+onlyinScot = set_Scot - set_NI - set_EngWales
+onlyinEng = set_EngWales - set_NI - set_Scot
+
+print("\n","Only in Northern Ireland:", onlyinNI,"\n")
+print("Only in Scotland:", onlyinScot,"\n")
+print("Only in England:",onlyinEng,"\n")
+
+#print("Only in Northern Ireland:",onlyinNI)
+
 
 #for dates in titles:
     #list(dates.keys())[0]
